a64a93466a10279e1d5c3bd6a81d40d69072779e
daveallisoncpc-del <dave.allisoncpc@gmail.com>
2026-07-07 09:18:50 +0100

    Expose Amelia booking config

--- api/config.js
+++ api/config.js
@@ -1,6 +1,7 @@
 export default function handler(request, response) {
   response.status(200).json({
     supabaseUrl: process.env.SUPABASE_URL || "",
-    supabaseAnonKey: process.env.SUPABASE_ANON_KEY || ""
+    supabaseAnonKey: process.env.SUPABASE_ANON_KEY || "",
+    ameliaBookingUrl: process.env.AMELIA_BOOKING_URL || "https://allisondriveracademy.com/enquiry/"
   });
 }
