b09d9f59df2fa99abc88084c25593adda3853312
daveallisoncpc-del <dave.allisoncpc@gmail.com>
2026-07-08 07:32:00 +0100

    Add approvedLearnerBookingUrl to config handler

--- api/config.js
+++ api/config.js
@@ -2,6 +2,7 @@ export default function handler(request, response) {
   response.status(200).json({
     supabaseUrl: process.env.SUPABASE_URL || "",
     supabaseAnonKey: process.env.SUPABASE_ANON_KEY || "",
-    ameliaBookingUrl: process.env.AMELIA_BOOKING_URL || "https://allisondriveracademy.com/enquiry/"
+    ameliaBookingUrl: process.env.AMELIA_BOOKING_URL || "https://allisondriveracademy.com/enquiry/",
+    approvedLearnerBookingUrl: process.env.AMELIA_APPROVED_BOOKING_URL || process.env.AMELIA_BOOKING_URL || "https://allisondriveracademy.com/enquiry/"
   });
 }
