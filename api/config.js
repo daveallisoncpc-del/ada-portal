f021c770f0a3593451a1dffaabe2fe187138635e
daveallisoncpc-del <dave.allisoncpc@gmail.com>
2026-07-08 07:37:50 +0100

    Add paymentRedirectUrl to config response
    Added paymentRedirectUrl to the response object.

--- api/config.js
+++ api/config.js
@@ -3,6 +3,7 @@ export default function handler(request, response) {
     supabaseUrl: process.env.SUPABASE_URL || "",
     supabaseAnonKey: process.env.SUPABASE_ANON_KEY || "",
     ameliaBookingUrl: process.env.AMELIA_BOOKING_URL || "https://allisondriveracademy.com/enquiry/",
-    approvedLearnerBookingUrl: process.env.AMELIA_APPROVED_BOOKING_URL || process.env.AMELIA_BOOKING_URL || "https://allisondriveracademy.com/enquiry/"
+    approvedLearnerBookingUrl: process.env.AMELIA_APPROVED_BOOKING_URL || process.env.AMELIA_BOOKING_URL || "https://allisondriveracademy.com/enquiry/",
+    paymentRedirectUrl: process.env.PAYMENT_REDIRECT_URL || "/api/payment-link"
   });
 }
